d3d7b418e6394c8cf29b6b4b03e98288bcaf3816
phucvin <phucvin@gmail.com>
2022-11-19 20:21:41 +0000

    call some external api

--- services/hello.go
+++ services/hello.go
@@ -3,8 +3,10 @@ package hello
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"sync"
 
 	"call"
 )
@@ -25,12 +27,38 @@ func callRandom() RandomRes {
 	return res
 }
 
+func callSomeExternalApi() (string, error) {
+	res, err := http.Get("https://randomuser.me/api/")
+	if err != nil {
+		return "", err
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(resBody), nil
+}
+
 func HandleHTTP(w http.ResponseWriter, req *http.Request) {
-	randomRes := callRandom()
-	if randomRes.Error != nil {
-		fmt.Fprintf(w, "error getting a random name to say hello")
+	var randomRes RandomRes
+	var externalRes string
+	var externalErr error
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		randomRes = callRandom()
+		wg.Done()
+	}()
+	go func() {
+		externalRes, externalErr := callSomeExternalApi()
+		wg.Done()
+	}()
+	wg.Wait()
+
+	if randomRes.Error != nil || externalErr != nil {
+		fmt.Fprintf(w, "error getting a random name to say hello\n\n%v\n\n%v", randomRes.Error, externalErr)
 	} else {
-		fmt.Fprintf(w, "hello "+*randomRes.RandomValue)
+		fmt.Fprintf(w, "hello %s\n\nuser:\n%s", *randomRes.RandomValue, externalRes)
 	}
 }
 
